7dd226436cec1bfcc37e345c9db389877ef3a839
seadynamic8 <seadynamic8@zoho.com>
2023-10-27 12:03:18 -0400

    feat: added logger setup for future use

---
+++ app/lib/utils/logger.dart
@@ -0,0 +1,10 @@
+import 'package:logger/logger.dart';
+
+final logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 0,
+    errorMethodCount: 8,
+    lineLength: 60,
+    noBoxingByDefault: true,
+  ),
+);
