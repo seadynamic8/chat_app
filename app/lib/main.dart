a39276ce6ee0ec58f75b85833b8bd769f6e06151
seadynamic8 <seadynamic8@zoho.com>
2023-10-27 15:02:53 -0400

    Changed title to 'Chat with Friends'

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends ConsumerWidget {
     final appRouter = ref.watch(appRouterProvider);
 
     return MaterialApp.router(
-      title: 'Flutter Demo',
+      title: 'Chat With Friends',
       darkTheme: ThemeData.dark()
           .copyWith(useMaterial3: true, colorScheme: const ColorScheme.dark()),
       themeMode: ThemeMode.dark,
