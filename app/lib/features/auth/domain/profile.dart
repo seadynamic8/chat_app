ed3e207595e525b1a887b4bb9c4dcafaf54500a8
seadynamic8 <seadynamic8@zoho.com>
2023-10-31 16:49:13 -0400

    refactor: add comment to explain why profile and user (or currentUser) is often used interchangably.

--- app/lib/features/auth/domain/profile.dart
+++ app/lib/features/auth/domain/profile.dart
@@ -1,6 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+// * Database (Supabase) Stores Auth User and Profile in seperate tables
+// * Auth User (id, email, encrypted_password)
+// * Profile User (id, username, avatar_url, etc...)
+// * Profile id is linked to Auth User Id
+// * --> so that means, we often use User or Profile to mean the same thing
+
 class Profile {
   const Profile({
     required this.id,
