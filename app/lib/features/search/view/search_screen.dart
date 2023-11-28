d06cde4758c20d3057f49ab0b60f64a702eeef19
seadynamic8 <seadynamic8@zoho.com>
2023-11-28 11:17:40 -0500

    feat: add online status indicator to search profiles results

--- app/lib/features/search/view/search_screen.dart
+++ app/lib/features/search/view/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:chat_app/common/async_value_widget.dart';
+import 'package:chat_app/features/chat/view/chat_online_status_icon.dart';
 import 'package:chat_app/features/search/view/search_controller.dart';
 import 'package:chat_app/routing/app_router.gr.dart';
 import 'package:chat_app/utils/constants.dart';
@@ -62,19 +63,28 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
             value: profilesValue,
             data: (profiles) => ListView.builder(
               itemCount: profiles.length,
-              itemBuilder: (context, index) => ListTile(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  backgroundImage: AssetImage(
-                      profiles[index].avatarUrl ?? defaultAvatarImage),
-                  radius: 25,
-                ),
-                title: Text(profiles[index].username ?? ''),
-                onTap: () => context.router
-                    .push(PublicProfileRoute(profileId: profiles[index].id!)),
-              ),
+              itemBuilder: (context, index) {
+                final profile = profiles[index];
+
+                return ListTile(
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                  leading: Stack(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        backgroundImage:
+                            AssetImage(profile.avatarUrl ?? defaultAvatarImage),
+                        radius: 20,
+                      ),
+                      ChatOnlineStatusIcon(userId: profile.id!)
+                    ],
+                  ),
+                  title: Text(profile.username ?? ''),
+                  onTap: () => context.router
+                      .push(PublicProfileRoute(profileId: profile.id!)),
+                );
+              },
             ),
           ),
         ),
