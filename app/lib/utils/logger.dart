366e9b0816ed836e263b98cc805c5bc9411b1a5e
seadynamic8 <seadynamic8@zoho.com>
2023-12-27 13:40:56 -0500

    chore/style: add custom logger printer to see class names printed before log messages (other than for errors)

--- app/lib/utils/logger.dart
+++ app/lib/utils/logger.dart
@@ -11,3 +11,34 @@ final logger = Logger(
     },
   ),
 );
+
+// * Get custom logger that will display class name with messages
+Logger getLogger(String className) {
+  return Logger(printer: MyPrinter(className: className));
+}
+
+class MyPrinter extends PrettyPrinter {
+  final String className;
+
+  MyPrinter({this.className = ''})
+      : super(
+          methodCount: 1,
+          errorMethodCount: 8,
+          lineLength: 60,
+          noBoxingByDefault: true,
+          excludeBox: {
+            Level.error: false,
+          },
+        );
+
+  @override
+  List<String> log(LogEvent event) {
+    if (event.level == Level.error) return super.log(event);
+
+    final color = PrettyPrinter.defaultLevelColors[event.level];
+    final emoji = PrettyPrinter.defaultLevelEmojis[event.level];
+    final errorString = '$emoji $className - ${event.message}';
+    return [color == null ? errorString : color(errorString)];
+  }
+}
+
