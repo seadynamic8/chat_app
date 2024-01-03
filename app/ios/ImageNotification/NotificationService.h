ff45c75979054414a9bd786d35660ddb031f80ff
seadynamic8 <seadynamic8@zoho.com>
2024-01-03 14:05:16 -0500

    chore: ios configuration for firebase messaging

---
+++ app/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Jarvis Kwan on 1/3/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
