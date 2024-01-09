e731ec02134cdc36aa402314a9db516b585ef2a0
seadynamic8 <seadynamic8@zoho.com>
2024-01-09 12:04:09 -0500

    refactor: run formatter on edge function

--- supabase/functions/create_notification/index.ts
+++ supabase/functions/create_notification/index.ts
@@ -1,30 +1,29 @@
-import { corsHeaders } from '../_shared/cors.ts'
-import { supabaseAdmin } from '../_shared/supabaseAdmin.ts'
-import serviceAccount from '../service-account.json' with { type: 'json' }
-import { JWT } from 'npm:google-auth-library@9'
+import { corsHeaders } from "../_shared/cors.ts";
+import { supabaseAdmin } from "../_shared/supabaseAdmin.ts";
+import serviceAccount from "../service-account.json" with { type: "json" };
+import { JWT } from "npm:google-auth-library@9";
 // import { apnsPrivateKey } from '../AuthKey.ts'
 // import { create, getNumericDate } from 'https://deno.land/x/djwt@v3.0.1/mod.ts'
 
-console.log("Running create notification function")
+console.log("Running create notification function");
 
 Deno.serve(async (req) => {
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
   }
-  const { otherProfileId, notification, data: params } = await req.json()
+  const { otherProfileId, notification, data: params } = await req.json();
 
   const fcmTokens = await getFCMTokens(otherProfileId);
 
   const googleAccessToken = await getAccessToken({
     clientEmail: serviceAccount.client_email,
-    privateKey: serviceAccount.private_key
-  })
+    privateKey: serviceAccount.private_key,
+  });
   // const apnsJWT = await getAPNSJWT();
 
-  console.log('Sending fcm message to other user....')
-  
+  console.log("Sending fcm message to other user....");
+
   fcmTokens.forEach(async (token, _idx, _tokens) => {
-    
     let apnsSettings;
     if (token.apns != null) {
       apnsSettings = {
@@ -36,114 +35,111 @@ Deno.serve(async (req) => {
         //   'apns-push-type': 'alert',
         //   'apns-id': token.apns, // a unique id to reference notification for errors
         // },
-        'payload': { 
-          'aps': {
-            'sound': 'default'
-          }
-        }, 
-      }
+        "payload": {
+          "aps": {
+            "sound": "default",
+          },
+        },
+      };
     }
-    
+
     try {
       const res = await fetch(
         `https://fcm.googleapis.com/v1/projects/${serviceAccount.project_id}/messages:send`,
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${googleAccessToken}`,
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${googleAccessToken}`,
           },
           body: JSON.stringify({
             message: {
               token: token.fcm,
               android: {
-                priority: "high"
+                priority: "high",
               },
               apns: apnsSettings,
               notification: notification,
               data: {
-                'otherProfileId': otherProfileId,
-                ...params
-              }
-            }
-          })
-        }
-      )
-      const resData = await res.json()
+                "otherProfileId": otherProfileId,
+                ...params,
+              },
+            },
+          }),
+        },
+      );
+      const resData = await res.json();
 
-      if (res.status < 200 || 299 < res.status ) {
-        throw resData
+      if (res.status < 200 || 299 < res.status) {
+        throw resData;
       }
     } catch (error) {
       // Token either was refreshed or uninstalled or etc, anyways invalid now
-      if (error.error.status == 'UNREGISTERED') {
+      if (error.error.status == "UNREGISTERED") {
         // Delete token
         await deleteFCMToken(token.fcm);
       }
-      throw error
+      throw error;
     }
-  })
+  });
 
-  console.log('Finished sending message, returning....')
+  console.log("Finished sending message, returning....");
 
   return new Response(
     JSON.stringify("ok"),
     { headers: { "Content-Type": "application/json" } },
-  )
-})
+  );
+});
 
 interface Token {
-  fcm: string
-  apns: null | string
+  fcm: string;
+  apns: null | string;
 }
 
-const getFCMTokens = async (otherProfileId: string): 
-    Promise<Token[]> => {
-  const { data: fcmTokens, error: fcmError } = 
-    await supabaseAdmin
-      .rpc('fcm_tokens_for_user', { 'profile_id': otherProfileId })
+const getFCMTokens = async (otherProfileId: string): Promise<Token[]> => {
+  const { data: fcmTokens, error: fcmError } = await supabaseAdmin
+    .rpc("fcm_tokens_for_user", { "profile_id": otherProfileId });
 
   if (fcmError != null) {
-    console.log('fcmError');
-    throw fcmError
+    console.log("fcmError");
+    throw fcmError;
   }
   return fcmTokens;
-}
+};
 
 const deleteFCMToken = async (token: string) => {
-  const { error } = 
-    await supabaseAdmin
-      .rpc('delete_fcm_token', { 'token': token })
+  const { error } = await supabaseAdmin
+    .rpc("delete_fcm_token", { "token": token });
 
   if (error != null) {
-    console.log('error');
-    throw error
+    console.log("error");
+    throw error;
   }
-}
+};
 
 const getAccessToken = ({
   clientEmail,
   privateKey,
 }: {
-  clientEmail: string
-  privateKey: string
+  clientEmail: string;
+  privateKey: string;
 }): Promise<string> => {
   return new Promise((resolve, reject) => {
     const jwtClient = new JWT({
       email: clientEmail,
       key: privateKey,
-      scopes: ['https://www.googleapis.com/auth/firebase.messaging'],
-    })
+      scopes: ["https://www.googleapis.com/auth/firebase.messaging"],
+    });
 
     jwtClient.authorize((err, tokens) => {
       if (err) {
-        reject(err)
-        return
+        reject(err);
+        return;
       }
-      resolve(tokens!.access_token!)
-    })
-  })
-}
+      resolve(tokens!.access_token!);
+    });
+  });
+};
 
 // function parsePem(pem: string) {
 //   const pemHeader = "-----BEGIN PRIVATE KEY-----";
