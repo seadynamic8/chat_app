35cbdf5f9a0efe68d5091a5ebb3751e843f02dc5
seadynamic8 <seadynamic8@zoho.com>
2024-01-13 19:50:09 -0500

    fix: change the message timestamp to db autogenerated one, so that it fixes the time

--- app/lib/features/chat/data/chat_repository.dart
+++ app/lib/features/chat/data/chat_repository.dart
@@ -55,7 +55,6 @@ class ChatRepository {
             'content': message.content,
             'profile_id': message.profileId,
             'room_id': roomId,
-            'created_at': DateTime.now().toIso8601String(),
           })
           .select<Map<String, dynamic>>()
           .single();
