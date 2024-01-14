fed0d262a120ea616393f687f0cf0f7b171b8bca
seadynamic8 <seadynamic8@zoho.com>
2024-01-14 14:50:56 -0500

    fix: add missing existing tables to realtime

---
+++ supabase/migrations/20240114194935_enable_existing_tables_for_realtime.sql
@@ -0,0 +1,7 @@
+ALTER PUBLICATION supabase_realtime ADD table public.access_levels;
+ALTER PUBLICATION supabase_realtime ADD table public.blocked_users;
+ALTER PUBLICATION supabase_realtime ADD table public.chat_users;
+ALTER PUBLICATION supabase_realtime ADD table public.fcm_tokens;
+ALTER PUBLICATION supabase_realtime ADD table public.messages;
+ALTER PUBLICATION supabase_realtime ADD table public.messages_users;
+ALTER PUBLICATION supabase_realtime ADD table public.profiles;
